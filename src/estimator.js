eeb36798d70f235c4f6e4a92ead8eb5970069509
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2020-04-07 17:52:54 +0100

    Added function to calculate days depending on periodType supplied

--- src/estimator.js
+++ src/estimator.js
@@ -8,12 +8,14 @@ const covid19ImpactEstimator = (data) => {
   severeImpact.currentlyInfected = data.reportedCases * 50;
 
   impact.infectionsByRequestedTime = getProjectedInfections(
-    28,
+    data.timeToElapse,
+    data.periodType,
     impact.currentlyInfected
   );
 
   severeImpact.infectionsByRequestedTime = getProjectedInfections(
-    28,
+    data.timeToElapse,
+    data.periodType,
     severeImpact.currentlyInfected
   );
 
@@ -25,3 +27,17 @@ const covid19ImpactEstimator = (data) => {
 };
 
 export default covid19ImpactEstimator;
+
+// const input = {
+//   region: {
+//     name: 'Africa',
+//     avgAge: 19.7,
+//     avgDailyIncomeInUSD: 5,
+//     avgDailyIncomePopulation: 0.71
+//   },
+//   periodType: 'days',
+//   timeToElapse: 58,
+//   reportedCases: 674,
+//   population: 66622705,
+//   totalHospitalBeds: 1380614
+// };
--- src/helpers.js
+++ src/helpers.js
@@ -1,5 +1,21 @@
-export default function getProjectedInfections(duration, currentlyInfected) {
-  const factor = Math.floor(duration / 3);
+function resolveToDays(dateFormat, duration) {
+  switch (dateFormat) {
+    case 'months':
+      return duration * 30;
+    case 'weeks':
+      return duration * 7;
+    default:
+      return duration;
+  }
+}
+
+export default function getProjectedInfections(
+  duration,
+  periodType,
+  currentlyInfected
+) {
+  const resolvedDuration = resolveToDays(periodType, duration);
+  const factor = Math.floor(resolvedDuration / 3);
 
   return currentlyInfected * 2 ** factor;
 }
