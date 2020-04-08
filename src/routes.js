1f9c66337948bc8ff7298b0f75a4fb014d430391
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2020-04-08 18:41:59 +0100

    Setup server to respond with xml

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,21 @@
 import { Router } from 'express';
+import xml2js from 'xml2js';
 import covid19ImpactEstimator from './estimator';
 
 const router = Router();
+
+router.post('/xml', (req, res) => {
+  const inputData = req.body;
+  const result = covid19ImpactEstimator(inputData);
+
+  const builder = new xml2js.Builder({
+    renderOpts: { pretty: true }
+  });
+
+  const xmlString = builder.buildObject(result);
+  res.set(('Content-Type', 'text/xml'));
+  res.status(200).send(xmlString);
+});
 
 router.post('/json', (req, res) => {
   const inputData = req.body;
