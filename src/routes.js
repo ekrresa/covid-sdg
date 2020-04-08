e041f075c19ecf1f809c4be89fdbc0a70b2b7d92
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2020-04-08 14:13:52 +0100

    Add route for json response

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,12 @@ import covid19ImpactEstimator from './estimator';
 
 const router = Router();
 
+router.post('/json', (req, res) => {
+  const inputData = req.body;
+  const result = covid19ImpactEstimator(inputData);
+  res.status(200).send(result);
+});
+
 router.post('/', (req, res) => {
   const inputData = req.body;
   const result = covid19ImpactEstimator(inputData);
