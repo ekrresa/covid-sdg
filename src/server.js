6b439cd138f0b37d2548d3d9ce6c1010ffc3dd47
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2020-04-08 13:25:16 +0100

    Setup server to handle requests

---
+++ src/server.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import http from 'http';
+import helmet from 'helmet';
+import cors from 'cors';
+import compression from 'compression';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+
+dotenv.config();
+const app = express();
+
+const { NODE_ENV, PORT } = process.env;
+const logFormat = NODE_ENV === 'production' ? 'combined' : 'dev';
+
+app.use(
+  morgan(logFormat, {
+    skip(_req, res) {
+      if (NODE_ENV === 'test') {
+        return true;
+      }
+
+      return res.statusCode <= 500;
+    },
+    stream: process.stderr
+  })
+);
+
+app.use(cors());
+app.use(helmet());
+app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (_req, res) => res.json({ data: 'hello world' }));
+
+http.createServer(app).listen(PORT, console.log(`server listening on ${PORT}`));
