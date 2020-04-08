71f1b022031266097b6d82b32097a6306f016d59
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2020-04-08 09:09:51 +0100

    Refactored each impact estimator to its own file

--- src/estimator.js
+++ src/estimator.js
@@ -1,55 +1,9 @@
-import getProjectedInfections, { resolveToDays } from './helpers';
+import impactEstimator from './impact';
+import severeImpactEstimator from './severeImpact';
 
 const covid19ImpactEstimator = (data) => {
-  const impact = {};
-  const severeImpact = {};
-
-  impact.currentlyInfected = data.reportedCases * 10;
-  severeImpact.currentlyInfected = data.reportedCases * 50;
-
-  impact.infectionsByRequestedTime = getProjectedInfections(
-    data.timeToElapse,
-    data.periodType,
-    impact.currentlyInfected
-  );
-
-  severeImpact.infectionsByRequestedTime = getProjectedInfections(
-    data.timeToElapse,
-    data.periodType,
-    severeImpact.currentlyInfected
-  );
-
-  impact.severeCasesByRequestedTime = 0.15 * impact.infectionsByRequestedTime;
-  severeImpact.severeCasesByRequestedTime =
-    0.15 * severeImpact.infectionsByRequestedTime;
-
-  const bedsAvailable = 0.35 * data.totalHospitalBeds;
-
-  impact.hospitalBedsByRequestedTime =
-    bedsAvailable - impact.severeCasesByRequestedTime;
-  severeImpact.hospitalBedsByRequestedTime =
-    bedsAvailable - severeImpact.severeCasesByRequestedTime;
-
-  impact.casesForICUByRequestedTime = 0.05 * impact.infectionsByRequestedTime;
-  severeImpact.casesForICUByRequestedTime =
-    0.05 * severeImpact.infectionsByRequestedTime;
-
-  impact.casesForVentilatorsByRequestedTime =
-    0.02 * impact.infectionsByRequestedTime;
-  severeImpact.casesForVentilatorsByRequestedTime =
-    0.02 * severeImpact.infectionsByRequestedTime;
-
-  impact.dollarsInFlight =
-    impact.infectionsByRequestedTime *
-    data.region.avgDailyIncomePopulation *
-    data.region.avgDailyIncomeInUSD *
-    resolveToDays(data.periodType, data.timeToElapse);
-
-  severeImpact.dollarsInFlight =
-    severeImpact.infectionsByRequestedTime *
-    data.region.avgDailyIncomePopulation *
-    data.region.avgDailyIncomeInUSD *
-    resolveToDays(data.periodType, data.timeToElapse);
+  const impact = impactEstimator(data);
+  const severeImpact = severeImpactEstimator(data);
 
   return {
     data: { ...data },
@@ -59,17 +13,3 @@ const covid19ImpactEstimator = (data) => {
 };
 
 export default covid19ImpactEstimator;
-
-// const input = {
-//   region: {
-//     name: 'Africa',
-//     avgAge: 19.7,
-//     avgDailyIncomeInUSD: 5,
-//     avgDailyIncomePopulation: 0.71
-//   },
-//   periodType: 'days',
-//   timeToElapse: 58,
-//   reportedCases: 674,
-//   population: 66622705,
-//   totalHospitalBeds: 1380614
-// };
---
+++ src/impact.js
@@ -0,0 +1,33 @@
+import getProjectedInfections, { resolveToDays } from './helpers';
+
+export default function impactEstimator(inputData) {
+  const output = {};
+
+  output.currentlyInfected = inputData.reportedCases * 10;
+
+  output.infectionsByRequestedTime = getProjectedInfections(
+    inputData.timeToElapse,
+    inputData.periodType,
+    output.currentlyInfected
+  );
+
+  output.severeCasesByRequestedTime = 0.15 * output.infectionsByRequestedTime;
+
+  const bedsAvailable = 0.35 * inputData.totalHospitalBeds;
+
+  output.hospitalBedsByRequestedTime =
+    bedsAvailable - output.severeCasesByRequestedTime;
+
+  output.casesForICUByRequestedTime = 0.05 * output.infectionsByRequestedTime;
+
+  output.casesForVentilatorsByRequestedTime =
+    0.02 * output.infectionsByRequestedTime;
+
+  output.dollarsInFlight =
+    output.infectionsByRequestedTime *
+    inputData.region.avgDailyIncomePopulation *
+    inputData.region.avgDailyIncomeInUSD *
+    resolveToDays(inputData.periodType, inputData.timeToElapse);
+
+  return output;
+}
