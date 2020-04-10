c299435e60302455fe9bcc3e881b606d80855de0
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2020-04-10 17:27:44 +0100

    Removed xml part

--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,15 @@
 import { Router } from 'express';
-import convert from 'xml-js';
 import covid19ImpactEstimator from './estimator';
 import { readFromFile } from './helpers';
 
 const router = Router();
 
-router.post('/xml', (req, res) => {
-  const inputData = req.body;
-  const result = covid19ImpactEstimator(inputData);
+// router.post('/xml', (req, res) => {
+//   const inputData = req.body;
+//   const result = covid19ImpactEstimator(inputData);
 
-  const xmlObj = convert.js2xml(result, {
-    compact: true,
-    ignoreComment: true,
-    spaces: 2
-  });
-
-  res.set(('Content-Type', 'text/xml'));
-  res.status(200).send(xmlObj);
-});
+//   res.set(('Content-Type', 'text/xml'));
+// });
 
 router.post('/json', (req, res) => {
   const inputData = req.body;
